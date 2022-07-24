bec17f4391ae6f01aa952b22687a99c2f8e265ba
Marcell Mihály <mihalymarcell86@gmail.com>
2022-07-24 17:04:33 +0100

    fix bug in stylesheet

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -5,16 +5,16 @@ function Advice(props) {
     return (
       <>
         <p className={scss.advice_id}>Advice #{props.advice.id}</p>
-        <p className={scss.advice_text}>
+        <h1 className={scss.advice_text}>
           <q>{props.advice.advice}</q>
-        </p>
+        </h1>
       </>
     );
   } else
     return (
       <>
         <p className={`${scss.advice_id} ${scss.error}`}>An error occurred!</p>
-        <p className={scss.advice_text}>Failed to get new advice.</p>
+        <h1 className={scss.advice_text}>Failed to get new advice.</h1>
       </>
     );
 }
